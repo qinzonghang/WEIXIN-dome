df178cff2c84882050a193162d3c259f850a1f5e
QINZONGHANG <694825691@qq.com>
2018-08-03 10:03:19 +0800

    map上线1图不全

--- city_map/pages/map/map.js
+++ city_map/pages/map/map.js
@@ -1,5 +1,26 @@
+import echarts from '../../ec-canvas/echarts'
+
+function initChart(canvas, width, height) {
+    const chart = echarts.init(canvas, null, {
+      width: width,
+      height: height
+    });
+    canvas.setChart(chart);
+  
+    var option = {
+        geo : {
+            map: 'china',
+        }
+    };
+    chart.setOption(option);
+    return chart;
+  }
+
 Page({
     data : {
+        ec: {
+            onInit: initChart
+          },
        list : [] ,
        province : [] , //省份
        m : 0 , //省份个数
